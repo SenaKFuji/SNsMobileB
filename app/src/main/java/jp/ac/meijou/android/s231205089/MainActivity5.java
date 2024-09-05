1f1c027395c37211a06c6fd1d89a23c0e0c1b3a0
Sena KFuji <SenaKFuji+SenaKFuji@users.noreply.github.com>
2024-09-05 14:42:29 +0900

    4日目その1 MainActivity4、5、6の作成。URLを読み込み、WebAPIによるWebからのテキストの読み込み、画像の読み込み。 また、近隣ネットワークの読み込み、ネットワークの確認とIPアドレスの確認機能の搭載。

---
+++ app/src/main/java/jp/ac/meijou/android/s231205089/MainActivity5.java
@@ -0,0 +1,97 @@
+package jp.ac.meijou.android.s231205089;
+
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import jp.ac.meijou.android.s231205089.databinding.ActivityMain5Binding;
+import jp.ac.meijou.android.s231205089.databinding.ActivityMainBinding;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity5 extends AppCompatActivity {
+
+    private final OkHttpClient okHttpClient = new OkHttpClient();
+    private final Moshi moshi = new Moshi.Builder().build();
+    private final JsonAdapter<Gist> gistJsonAdapter = moshi.adapter(Gist.class);
+    private ActivityMain5Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMain5Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding.button1.setOnClickListener(view -> {
+            var text = binding.textUrl.getText().toString();
+
+            var url = Uri.parse("https://placehold.jp/350x350.png")
+                    .buildUpon()
+                    .appendQueryParameter("text", text)
+                    .build()
+                    .toString();
+
+            getImage(url);
+        });
+
+        /*
+        var request = new Request.Builder()
+                .url()
+                .build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                var bitmap = BitmapFactory.decodeStream(response.body().byteStream());
+                runOnUiThread(() -> binding.image1.setImageBitmap(bitmap));
+            }
+        });
+        */
+    }
+
+    private void getImage(String url) {
+        var request = new Request.Builder()
+                .url(url)
+                .build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                var bitmap = BitmapFactory.decodeStream(response.body().byteStream());
+                runOnUiThread(() -> binding.image1.setImageBitmap(bitmap));
+            }
+        });
+    }
+}
